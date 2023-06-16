d515ac0bee1150207f928a9506adffa9b416b344
Yabuinu98 <k4uk4u@naver.com>
2023-06-16 15:52:03 +0900

    mevn

--- nodejs/4ex_form2/4ex_form2.js
+++ nodejs/4ex_form2/4ex_form2.js
@@ -15,27 +15,53 @@ app.get('/data', (req, res) => {
     const gender = req.query.gender
     const content = req.query.content
     const date = req.query.date
+
+    const arrT = ["나이", "성별", "자기소개", "가입일"]
+    const arrC = [age, gender, content, date]
+
     let list = ``
-    list += `<body style="background-color: beige;">`
+    // alert 부분
+    list += `<script>`
+    list += `alert("오늘은 ${date} ${name} 님 환영합니다.")`
+    list += `</script>`
+
+    // 표 부분
     list += `<div style="display: flex; align-items: center; flex-direction: column;">`
     list += `<h2><mark>${name}</mark>님 어서오세요</h2>`
     list += `<p style="background-color: red; color: white; font-weight: bold; display: inline-block;">${name}님의 개인정보</p>`
     list += `<table style="border-collapse: collapse; text-align: center;">`
-    list += `<tr style="border: 1px solid black;">
-    <td style="border: 1px solid black; background-color: plum;">나이</td>
-    <td style="border: 1px solid black;">${age}</td></tr>`
-    list += `<tr style="border: 1px solid black;">
-    <td style="border: 1px solid black; background-color: plum;">성별</td>
-    <td style="border: 1px solid black;">${gender}</td></tr>`
-    list += `<tr style="border: 1px solid black;">
-    <td style="border: 1px solid black; background-color: plum;">자기소개</td>
-    <td style="border: 1px solid black;">${content}</td></tr>`
-    list += `<tr style="border: 1px solid black;">
-    <td style="border: 1px solid black; background-color: plum;">가입일</td>
-    <td style="border: 1px solid black;">${date}</td></tr>`
+    for(let i = 0; i<arrC.length; i++){
+        list += `<tr style="border: 1px solid black;">
+        <td style="border: 1px solid black; background-color: plum;">${arrT[i]}</td>
+        <td style="border: 1px solid black;">${arrC[i]}</td></tr>`
+    }
     list += `</table>`
     list += `</div>`
-    list += `</body>`
+    
+    // 모달창
+    list += `<div class="modal" style="display: none">`
+    list += `<div id="modal_body">`
+    list += `<div id="name"><span>이름</span><span id="nameIn">${name}</span></div>`
+    list += `<div id="age"><span>나이</span><span id="ageIn">${age}</span></div>`
+    list += `<div id="gender"><span>성별</span><span id="genderIn">${gender}</span></div>`
+    list += `<div id="content"><span>내용</span><span id="contentIn">${content}</span></div>`
+    list += `<div id="date"><span>날짜</span><span id="dateIn">${date}</span></div>`
+    list += `<div id="close"><button id="modalclo">닫기</button></div>`
+    list += `</div></div>`
+    list += `<button id="modalop">모달창열기</button>`
+    // 모달창 스크립트 구문
+    list += `<script>
+        const modal = document.querySelector(".modal");
+        const btnop = document.getElementById("modalop");
+        const btnclo = document.getElementById("modalclo");
+        btnop.addEventListener("click", () => {
+            modal.style.display = "block";
+        });
+        btnclo.addEventListener("click", () => {
+            modal.style.display = "none";
+        });
+        </script>`
+
     res.send(list)
 })
 
