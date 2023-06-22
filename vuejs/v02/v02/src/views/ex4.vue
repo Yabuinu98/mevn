7d710a0e5f88221ee81279435887c96b7c384708
Yabuinu98 <k4uk4u@naver.com>
2023-06-22 10:48:42 +0900

    mevn

--- vuejs/v02/v02/src/views/ex4.vue
+++ vuejs/v02/v02/src/views/ex4.vue
@@ -1,5 +1,39 @@
 <template>
-  <div><h1>여기는 EX4 입니다.</h1></div>
+  <div>
+    <button v-for="p in pages" :key="p" :class="p" @click="page = p">
+      {{ p }}
+    </button>
+    <div v-for="(g, i) in pages" :key="i">
+      <!-- 이미지들을 pubilc폴더에 려야 링크를 걸기가 편하다(로컬호스트에 바로 붙는다) -->
+      <img v-if="g === page" :src="'./샘플' + (i + 1) + '.jpg'" alt="사진" />
+    </div>
+  </div>
 </template>
-<script></script>
-<style></style>
+<script>
+export default {
+  data() {
+    return {
+      page: 'page1',
+      pages: ['page1', 'page2', 'page3', 'page4']
+    }
+  }
+}
+</script>
+<style>
+button {
+  scale: 200%;
+  background-color: white;
+  border: 1px solid gray;
+  margin: 30px;
+  cursor: pointer;
+  box-shadow: 2px 2px black;
+}
+button:hover {
+  background-color: yellow;
+  box-shadow: inset 2px 2px black;
+}
+img {
+  width: 60%;
+  height: 50%;
+}
+</style>
---
+++ vuejs/v03/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>This is an Home page</h1>
+</template>
+<script></script>
+<style></style>
